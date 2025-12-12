37fae4af7d19e29945f4cf9e8d0b889ab626f47d
Juliewh <yuliamaryashina@gmail.com>
2025-12-12 08:15:13 +0300

    Update IOrderRepository.cs

--- FoodDeliverySystem/Services/IOrderRepository.cs
+++ FoodDeliverySystem/Services/IOrderRepository.cs
@@ -1,31 +1,15 @@
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using FoodDelivery.Models;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace FoodDelivery.Services
 {
-    public class InMemoryOrderRepository : IOrderRepository
+    public interface IOrderRepository
     {
-        private readonly ConcurrentDictionary<Guid, Order> _store = new ConcurrentDictionary<Guid, Order>();
-
-        public void Add(Order order)
-        {
-            _store[order.Id] = order;
-        }
-
-        public Order GetById(Guid id)
-        {
-            _store.TryGetValue(id, out var order);
-            return order;
-        }
-
-        public IEnumerable<Order> GetAll() => _store.Values.ToList();
-
-        public void Update(Order order)
-        {
-            _store[order.Id] = order;
-        }
+        void Add(Order order);
+        Order GetById(Guid id);
+        IEnumerable<Order> GetAll();
+        void Update(Order order);
     }
 }
+
